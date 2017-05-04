4a51b97fcbafecdb6bcbb508e51977009aa7222d
Ashley Shenton <Shent@Ashleys-Mac.local>
2017-05-04 17:59:38 +0100

    minor code changes, minor cosmetic changes and add angular storage

--- js/controller.js
+++ js/controller.js
@@ -8,15 +8,24 @@ angular.module("routeControllers", [])
     .controller("decksController", function() {
 
     })
-    .controller("formController", function($scope) {
-        var limit = 8;
-        $('label input').on('change', function(evt) {
-            if($("input[name='cards']:checked").length > limit) {
-                this.checked = false;
-                window.alert("You can only select 8 cards! If you would like to change a card, please de-select one first.");
-            } else {
-                $(this).siblings().toggleClass("checked");
-            }
+    .controller("formController", function($scope, store, $location) {
+        $(document).ready(function() {
+            var limit = 8;
+            $('label input').on('change', function(evt) {
+                if($("input[name='cards']:checked").length > limit) {
+                    this.checked = false;
+                    alert("You can only select 8 cards! If you would like to change a card, please de-select one first.");
+                } else {
+                    $(this).siblings().toggleClass("checked");
+                }
+            });
+            $('#submit-btn').click(function() {
+                checked = $("input[name='cards']:checked").length;
+                if(checked !== 8) {
+                    alert("You have selected less than 8 cards. Please select a total of 8 cards.");
+                    return false;
+                }
+            });
         });
     })
     .controller("faqController", function() {
